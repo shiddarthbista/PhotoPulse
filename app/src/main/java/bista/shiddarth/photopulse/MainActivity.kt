120afd3c2480fb2cca2ac1693cdeb667b2a268be
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2025-01-22 15:14:44 -0500

    Add images

--- app/src/main/java/bista/shiddarth/photopulse/MainActivity.kt
+++ app/src/main/java/bista/shiddarth/photopulse/MainActivity.kt
@@ -1,15 +1,19 @@
 package bista.shiddarth.photopulse
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import bista.shiddarth.photopulse.ui.theme.PhotoPulseTheme
 
@@ -19,29 +23,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PhotoPulseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PhotoItem(resId = R.drawable.city)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PhotoItem(resId: Int) {
+    val context = LocalContext.current as ComponentActivity
+
+    context.enableEdgeToEdge(
+        statusBarStyle = SystemBarStyle.dark(Color.BLACK),
+        navigationBarStyle = SystemBarStyle.dark(Color.BLACK)
     )
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Image(
+            painter = painterResource(id = resId),
+            contentDescription = "Photo",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    PhotoPulseTheme {
-        Greeting("Android")
-    }
+fun PhotoItemPreview() {
+    PhotoItem(resId = R.drawable.city)
 }
